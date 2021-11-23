638381b70ab9cd992659f78c2610f95e75d81266
Juan Jose Peralta <jperalta@pixel2html.net>
2021-11-23 15:07:01 -0300

    agregue estructura principal

---
+++ src/components/menu/index.js
@@ -0,0 +1,26 @@
+const Menu = () => {
+  return (
+    // Your menu here 👇🏻
+    <div className="menu__container">
+      <ul>
+        <li>
+          <a>INICIO</a>
+        </li>
+        <span></span>
+        <li>
+          <a>UBICACION</a>
+        </li>
+        <span></span>
+        <li>
+          <a>REGALOS</a>
+        </li>
+        <span></span>
+        <li>
+          <a>RSVP</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
