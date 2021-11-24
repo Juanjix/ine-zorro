ce35aaee11debd37502ed84959d5be4a426b977e
Juan Jose Peralta <jperalta@pixel2html.net>
2021-11-23 22:09:49 -0300

    agregue el menu mas la seccion evento

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import LayoutPrimary from "layouts/layout-primary";
+import image from "assets/images/evento/recursoRecurso 2@2x.png";
+import image2 from "assets/images/evento/recursoRecurso 1@2x.png";
 
 const Home = () => {
-  return <LayoutPrimary></LayoutPrimary>;
+  return (
+    <LayoutPrimary>
+      <div className="evento">
+        <h1>x dias para dejarlo todo</h1>
+        <img src={image} alt="ine y zorro" />
+        <h1>18.02.2022</h1>
+        <h1> | 17 HS | </h1>
+        <div className="direccion">
+          <img src={image2} alt="ine y zorro" />
+          <h1>La Herencia</h1>
+          <h2>Saravia 1799, pilar</h2>
+        </div>
+      </div>
+    </LayoutPrimary>
+  );
 };
 
 export default Home;
