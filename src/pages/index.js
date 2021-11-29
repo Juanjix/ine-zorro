d0d7321f81e91f09a3af9a10acd55b87aeb74f8b
Juan Jose Peralta <jperalta@pixel2html.net>
2021-11-29 17:52:40 -0300

    agregue section regalos

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,31 +9,31 @@ import logoHerencia from "assets/images/evento/recursoRecurso 1@2x.png";
 import secondTitle from "assets/images/como-llegar/recursoRecurso 12@2x.png";
 import mapa from "assets/images/como-llegar/recursoRecurso 11@2x.png";
 
-//imagenes seccion como llegar? PRIMERA FILA
-import image5 from "assets/images/como-llegar/recursoRecurso 4@2x.png";
-import image6 from "assets/images/como-llegar/recursoRecurso 3@2x.png";
-import hoja from "assets/images/como-llegar/recursoRecurso 7@2x.png";
+// //imagenes seccion como llegar? PRIMERA FILA
+// import image5 from "assets/images/como-llegar/recursoRecurso 4@2x.png";
+// import image6 from "assets/images/como-llegar/recursoRecurso 3@2x.png";
+// import hoja from "assets/images/como-llegar/recursoRecurso 7@2x.png";
 
-//imagenes seccion como llegar? SEGUNDA FILA
-import image7 from "assets/images/como-llegar/recursoRecurso 5@2x.png";
-import image8 from "assets/images/como-llegar/recursoRecurso 6@2x.png";
-import hoja2 from "assets/images/como-llegar/recursoRecurso 8@2x.png";
+// //imagenes seccion como llegar? SEGUNDA FILA
+// import image7 from "assets/images/como-llegar/recursoRecurso 5@2x.png";
+// import image8 from "assets/images/como-llegar/recursoRecurso 6@2x.png";
+// import hoja2 from "assets/images/como-llegar/recursoRecurso 8@2x.png";
 
-//imagenes seccion como llegar? TERCERA FILA
-import image9 from "assets/images/como-llegar/recursoRecurso 9@2x.png";
-import image10 from "assets/images/como-llegar/recursoRecurso 10@2x.png";
+// //imagenes seccion como llegar? TERCERA FILA
+// import image9 from "assets/images/como-llegar/recursoRecurso 9@2x.png";
+// import image10 from "assets/images/como-llegar/recursoRecurso 10@2x.png";
 
-//imagenes regalo
-import tituloRegalo from "assets/images/regalos/recursoRecurso 13@2x.png";
+// //imagenes regalo
+import thirdTitle from "assets/images/regalos/recursoRecurso 13@2x.png";
 import travelPhoto from "assets/images/regalos/recursoRecurso 14@2x.png";
-import regalo1 from "assets/images/regalos/recursoRecurso 18@2x.png";
-import regalo2 from "assets/images/regalos/recursoRecurso 16@2x.png";
-import regalo3 from "assets/images/regalos/recursoRecurso 17@2x.png";
-import regalo4 from "assets/images/regalos/recursoRecurso 15@2x.png";
+// import regalo1 from "assets/images/regalos/recursoRecurso 18@2x.png";
+// import regalo2 from "assets/images/regalos/recursoRecurso 16@2x.png";
+// import regalo3 from "assets/images/regalos/recursoRecurso 17@2x.png";
+// import regalo4 from "assets/images/regalos/recursoRecurso 15@2x.png";
 
-//imagenes te esperamos
-import tituloTeEsperamos from "assets/images/te-esperamos/recursoRecurso 20@2x.png";
-import cheers from "assets/images/te-esperamos/recursoRecurso 22@2x.png";
+// //imagenes te esperamos
+// import tituloTeEsperamos from "assets/images/te-esperamos/recursoRecurso 20@2x.png";
+// import cheers from "assets/images/te-esperamos/recursoRecurso 22@2x.png";
 const Home = () => {
   return (
     <LayoutPrimary>
@@ -61,7 +61,7 @@ const Home = () => {
         {/* INICIO SECTION END */}
 
         {/* COMO LLEGAR SECTION START */}
-        <section className="como-llegar--container" id="como-llegar">
+        <section className="como-llegar__container" id="como-llegar">
           <img src={secondTitle} alt="ana y zorro" className="second--title" />
 
           <div className="mapa">
@@ -69,6 +69,29 @@ const Home = () => {
           </div>
         </section>
         {/* COMO LLEGAR SECTION END */}
+
+        {/* REGALOS SECTION START */}
+        <section className="regalos__container" id="regalos">
+          <img src={thirdTitle} alt="ana y zorro" className="third--title" />
+          <img src={travelPhoto} alt="ine y zorro" className="travel--photo" />
+          <div className="como-regalar">
+            <h2>
+              CUALQUIER REGALO QUE RECIBAMOS LO VAMOS
+              <br /> A USAR PARA IRNOS DE LUNA DE MIEL.
+            </h2>
+            <br></br>
+            <br></br>
+            <h2>BANCO ICBC</h2>
+            <h2>TITULAR DE LA VEGA DIEGO EZEQUIEL</h2>
+            <h2>ALIAS: INEYZORRO</h2>
+            <h2>CA$ 05010123788809</h2>
+            <h2>CBU: 0150501601000237888096</h2>
+            <br />
+            <br />
+            <h1>¡MUCHISIMAS GRACIAS!</h1>
+          </div>
+        </section>
+        {/* REGALOS SECTION END */}
       </div>
     </LayoutPrimary>
   );
